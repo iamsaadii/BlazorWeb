2039e0d30203a1168b6969b4073eb6444741fe6b
iamsaadi <saadimtiaz7864@gmail.com>
2024-08-18 03:27:04 +0500

    BlazorAppAdded

---
+++ UserInfoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CrudBlazorView
+{
+    public class UserInfoDTO
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
