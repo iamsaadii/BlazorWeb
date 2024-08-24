c390179e0baa9a13c6fbe6fc550a4761f058a6df
iamsaadi <saadimtiaz7864@gmail.com>
2024-08-25 03:01:31 +0500

    HardCode

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44345/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7255/") });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
